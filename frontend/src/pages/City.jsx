dbceb497a2a9623fed355f875f49d264e97bf004
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-23 17:22:13 +0000

    Added API call and props to recommendation table

--- frontend/src/pages/City.jsx
+++ frontend/src/pages/City.jsx
@@ -1,14 +1,32 @@
 import Map from "../components/Map";
 import RecommendationTable from "../components/RecommendationTable";
+import { useState, useEffect } from "react";
+
+import PropTypes from "prop-types";
 
 function City({ city_name, latitude, longitude }) {
+  const [recommendations, setRecommendations] = useState([]);
+
+  // API call to get recommendations
+  useEffect(() => {
+    fetch("http://localhost:5000/recommendation")
+      .then((response) => response.json())
+      .then((data) => setRecommendations(data));
+  }, []);
+
   return (
     <>
       <h1>{city_name ? city_name : "City name"}</h1>
       <Map latitude={latitude} longitude={longitude} />
-      <RecommendationTable />
+      <RecommendationTable recommendations={recommendations} />
     </>
   );
 }
+
+City.propTypes = {
+  city_name: PropTypes.string.isRequired,
+  latitude: PropTypes.string.isRequired,
+  longitude: PropTypes.string.isRequired,
+};
 
 export default City;
