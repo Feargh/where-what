22cf470cef1a551d81db5bcde1f6dd1be810d2dd
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-23 17:23:00 +0000

    Removed email newsletter sign up

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,35 +1,8 @@
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBInput,
-  MDBBtn,
-} from "mdb-react-ui-kit";
+import { MDBFooter } from "mdb-react-ui-kit";
 
 function Footer() {
   return (
     <MDBFooter className='text-center text-lg-left mt-5 bg-body-tertiary'>
-      <MDBContainer className='p-4 pb-0'>
-        <form action=''>
-          <MDBRow className='d-flex justify-content-center'>
-            <MDBCol size='auto' className='mb-4 mb-md-0'>
-              <p className='pt-2'>
-                <strong>Sign up for our newsletter</strong>
-              </p>
-            </MDBCol>
-
-            <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
-              <MDBInput type='text' id='form5Example2' label='Email address' />
-            </MDBCol>
-
-            <MDBCol size='auto' className='mb-4 mb-md-0'>
-              <MDBBtn>Subscribe</MDBBtn>
-            </MDBCol>
-          </MDBRow>
-        </form>
-      </MDBContainer>
-
       <div className='text-center p-3 bg-body-secondary'>
         &copy; {new Date().getFullYear()} Copyright: Where What
       </div>
