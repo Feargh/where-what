9124e7d48db4f04ffb4c5a18eb462b787a0ae955
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-23 17:23:30 +0000

    Added city

--- backend/RecommendationDb.cs
+++ backend/RecommendationDb.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-class RecommendationDb : DbContext
-{
-    public RecommendationDb(DbContextOptions<RecommendationDb> options)
-        : base(options) { }
 
-    public DbSet<Recommendation> Recommendations => Set<Recommendation>();
+namespace RecommendationList.Models
+{
+    public class RecommendationDb : DbContext
+    {
+        public RecommendationDb(DbContextOptions<RecommendationDb> options) : base(options) { }
+        public DbSet<Recommendation> Recommendations { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<City>()
+        //        .HasMany(c => c.Recommendations)
+        //        .WithOne(r => r.City)
+        //        .HasForeignKey(r => r.CityId);
+        //}
+    }
+
 }
