5ccc81859864dda8b1cbc37bac46fe9b1319b7f3
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-23 17:19:13 +0000

    Added city model

---
+++ backend/City.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace RecommendationList.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Latitude { get; set; }
+        public string? Longitude { get; set; }
+        public ICollection<Recommendation>? Recommendations { get; set; }
+    }
+}
