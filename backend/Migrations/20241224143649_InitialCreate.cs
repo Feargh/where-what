88b5c707df7403a75d1edd2bcc72693acf725501
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-30 10:05:17 +0000

    Initial migration

---
+++ backend/Migrations/20241224143649_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace where_what.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<double>(type: "REAL", nullable: false),
+                    Longitude = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recommendations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<double>(type: "REAL", nullable: false),
+                    Longitude = table.Column<double>(type: "REAL", nullable: false),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recommendations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recommendations_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Latitude", "Longitude", "Name" },
+                values: new object[,]
+                {
+                    { 1, 53.343834999999999, -6.2637359999999997, "Dublin" },
+                    { 2, 51.513666000000001, -0.130055, "London" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Recommendations",
+                columns: new[] { "Id", "CityId", "Description", "Latitude", "Longitude", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, 1, "Victorian red brick pub with great atmosphere", 53.343834999999999, -6.2637359999999997, "The Stag's Head", "Pub" },
+                    { 2, 2, "Delicious Japanese food, don't miss the matcha french toast", 51.513666000000001, -0.130055, "Shack-Fuyu Soho", "Japanese restaurant" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recommendations_CityId",
+                table: "Recommendations",
+                column: "CityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recommendations");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
