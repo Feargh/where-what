34f966a1805bcd06c9a32f60ee6b03fb147510b3
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-30 10:02:01 +0000

    Made some parameters required

--- backend/City.cs
+++ backend/City.cs
@@ -6,9 +6,9 @@ namespace RecommendationList.Models
     public class City
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Latitude { get; set; }
-        public string? Longitude { get; set; }
-        public ICollection<Recommendation>? Recommendations { get; set; }
+        public required string Name { get; set; }
+        public required double Latitude { get; set; }
+        public required double Longitude { get; set; }
+        public ICollection<Recommendation>? Recommendations { get; set; } = new List<Recommendation>();
     }
 }
