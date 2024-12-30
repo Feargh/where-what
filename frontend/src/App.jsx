b69d1d2f59d46794ec089cfff0d14743072bae4e
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-30 14:22:37 +0000

    Added context and dynamic city page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,24 @@ import "./App.css";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import About from "./pages/About";
-import Dublin from "./pages/Dublin";
-import London from "./pages/London";
-import Lisbon from "./pages/Lisbon";
+import CityPage from "./pages/CityPage";
 import SuggestACity from "./pages/SuggestACity";
+import { CityProvider } from "./CityContext";
 
 function App() {
   return (
-    <>
+    <CityProvider>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
-            <Route path='dublin' element={<Dublin />} />
-            <Route path='london' element={<London />} />
-            <Route path='lisbon' element={<Lisbon />} />
+            <Route path='/city/:cityName' element={<CityPage />} />
             <Route path='suggestacity' element={<SuggestACity />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </CityProvider>
   );
 }
 
