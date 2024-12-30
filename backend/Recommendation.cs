7c2cd76b2db3ec6609bb5c13a48d31f94709483d
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-12-30 10:03:05 +0000

    Made some parameters required

--- backend/Recommendation.cs
+++ backend/Recommendation.cs
@@ -6,13 +6,13 @@ namespace RecommendationList.Models
     public class Recommendation
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Type { get; set; }
-        public string? Description { get; set; }
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public required string Description { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public int CityId { get; set; }
-        public City City { get; set; }
+        public int CityId { get; set; } // Foreign key
+        public City? City { get; set; } //Reference navigation property
 
     }
 }
